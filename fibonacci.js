58dc21c52c2f57c34356f2eef426072c490472d2
vladiyudi <plastikwood@gmail.com>
2022-06-20 18:37:40 +0300

    commiting 4th

--- fibonacci.js
+++ fibonacci.js
@@ -1,27 +1,24 @@
-// function fibonacci(fIndex){
-// for (let i=2; i<=fIndex; i++){
-// fSequence.push(fSequence[i-2]+fSequence[i-1])
-// }
-// return fSequence[fSequence.length-1]
-// }
 
-let fSequence
-let count
+let button = document.getElementById("fButton")
+let input
+let finalResult = document.getElementById("displayF")
 
-function runFibonacci () {
-    count=0
-    fSequence = [0,1]
-    fIndex = document.getElementById("fInput").value
-    document.getElementById("displayF").innerText=fibonacci(fIndex)
+
+button.addEventListener("click",runFibonacci)
+
+
+function runFibonacci(){
+    finalResult.innerText=" "
+    finalResult.classList.add("spinner-border")
+
+    input = document.getElementById("fInput").value
+
+    fetch(`http://localhost:5050/fibonacci/${input}`)
+    .then(response => response.json())
+    .then(data => {
+        finalResult.classList.remove("spinner-border")
+        document.getElementById("displayF").innerText=data.result
+    });
+
     document.getElementById("fInput").value=" "
-}
-
-document.getElementById("fButton").addEventListener("click", runFibonacci)
-
-function fibonacci (){
-    while (count <= fIndex){
-    fSequence.push(fSequence[fSequence.length-1]+fSequence[fSequence.length-2])
-    count++
-    fibonacci (fIndex)}
-    return fSequence[fSequence.length-3]
 }
