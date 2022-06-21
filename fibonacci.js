0cb6e613165f2a14725e03847edc3772629ac9a1
vladiyudi <plastikwood@gmail.com>
2022-06-21 19:52:42 +0300

    milestone5

--- fibonacci.js
+++ fibonacci.js
@@ -1,24 +1,39 @@
+let button = document.getElementById("fButton");
+let input = document.getElementById("fInput");
+let finalResult = document.getElementById("displayF");
+let error = document.querySelector(".error");
+let serverError = document.getElementById("serverError");
 
-let button = document.getElementById("fButton")
-let input
-let finalResult = document.getElementById("displayF")
+button.addEventListener("click", runFibonacci);
+input.addEventListener("focus", clearInput);
 
-
-button.addEventListener("click",runFibonacci)
-
-
-function runFibonacci(){
-    finalResult.innerText=" "
-    finalResult.classList.add("spinner-border")
-
-    input = document.getElementById("fInput").value
-
-    fetch(`http://localhost:5050/fibonacci/${input}`)
-    .then(response => response.json())
-    .then(data => {
-        finalResult.classList.remove("spinner-border")
-        document.getElementById("displayF").innerText=data.result
-    });
-
-    document.getElementById("fInput").value=" "
+function runFibonacci() {
+  if (input.value > 50) {
+    input.classList.add("red");
+    error.classList.remove("dNone");
+  } else {
+    finalResult.innerText = " ";
+    finalResult.classList.add("spinner-border");
+    fetch(`http://localhost:5050/fibonacci/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        finalResult.classList.remove("spinner-border");
+        document.getElementById("displayF").innerText = data.result;
+      });
+    try {
+      if (input.value === "42") throw "Server Error: 42 is meaning of life";
+    } catch (error) {
+        serverError.innerText = error
+        finalResult.classList.remove("spinner-border"); 
+    }
+  }
 }
+
+function clearInput() {
+  input.value = " ";
+  finalResult.innerText = " ";
+  error.classList.add("dNone");
+  input.classList.remove("red");
+  serverError.innerText = " ";
+}
+
