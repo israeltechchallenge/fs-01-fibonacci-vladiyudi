ed157a19b3094a9b07c1e5ac7bb059bb821bec99
vladiyudi <plastikwood@gmail.com>
2022-06-23 16:23:37 +0300

    the correct code is at milestone06

--- fibonacci.js
+++ fibonacci.js
@@ -3,48 +3,49 @@ let input = document.getElementById("fInput");
 let finalResult = document.getElementById("displayF");
 let error = document.querySelector(".error");
 let serverError = document.getElementById("serverError");
+let resultContainer = document.querySelector(".resultContainer");
+let spinnerResult = document.getElementById("spinnerResult");
 
 button.addEventListener("click", runFibonacci);
 input.addEventListener("focus", clearInput);
 
 function runFibonacci() {
+    if (input.value.length){
   if (input.value > 50) {
-    input.classList.add("red");
-    error.classList.remove("dNone");
+    input.classList.add("border-danger", "text-danger");
+    error.classList.remove("d-none");
   } else {
     finalResult.innerText = " ";
-    finalResult.classList.add("spinner-border");
+    finalResult.classList.add("spinner-border", "text-primary");
     fetch(`http://localhost:5050/fibonacci/${input.value}`)
-      .then((response) => {
-        try { if (!response.ok) 
-           {
-            throw Error (" ")
-        }
-        } 
-        catch (error){
-           error.massage = "Server Error: 42 is meaning of life"
-            console.log(error)
-           serverError.innerText = error.massage;
-           finalResult.classList.remove("spinner-border");
-        }
-        finally {
-            return response.json()
-        }
-  }
-      )
+    .then(res => {
+        if(res.ok) return res.json()
+        return res.text().then(text => {throw new Error(text)})
+      })
       .then((data) => {
-        finalResult.classList.remove("spinner-border");
-        document.getElementById("displayF").innerText = data.result;
-      });
-  }
+          finalResult.classList.remove("spinner-border", "text-primary", "font-weight-bold");
+          finalResult.classList.add("fs-4", "text-decoration-underline")
+          finalResult.innerText = data.result;
+        fetchResults();
+    })
+    .catch((err) => {
+        console.log("this is: " + err);
+        finalResult.classList.remove("spinner-border", "text-primary", "font-weight-bold");
+        finalResult.classList.add("text-danger")
+        finalResult.innerText=err
+    })
 }
+    }}
 
 function clearInput() {
   input.value = " ";
   finalResult.innerText = " ";
   error.classList.add("dNone");
-  input.classList.remove("red");
+  input.classList.remove("border-danger", "text-danger", "fs-6");
   serverError.innerText = " ";
+  finalResult.classList.remove("text-danger", "fs-4", "text-decoration-underline")
+  error.classList.add("d-none")
 }
+
 
 
